13d58df5ea9bd122ce3ed972760d69f46921bfb4
dim1000127 <60386475+dim1000127@users.noreply.github.com>
2023-06-11 18:55:15 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace MethodPotencialov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDownCol = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownRow = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.listBoxSteps = new System.Windows.Forms.ListBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonCreateMas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 20);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Потребности(bj)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(35, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 20);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Запасы(ai)";
+            // 
+            // numericUpDownCol
+            // 
+            this.numericUpDownCol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownCol.Location = new System.Drawing.Point(173, 71);
+            this.numericUpDownCol.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownCol.Name = "numericUpDownCol";
+            this.numericUpDownCol.Size = new System.Drawing.Size(51, 26);
+            this.numericUpDownCol.TabIndex = 33;
+            this.numericUpDownCol.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownCol.ValueChanged += new System.EventHandler(this.numericUpDownCol_ValueChanged);
+            // 
+            // numericUpDownRow
+            // 
+            this.numericUpDownRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownRow.Location = new System.Drawing.Point(173, 26);
+            this.numericUpDownRow.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownRow.Name = "numericUpDownRow";
+            this.numericUpDownRow.Size = new System.Drawing.Size(51, 26);
+            this.numericUpDownRow.TabIndex = 32;
+            this.numericUpDownRow.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownRow.ValueChanged += new System.EventHandler(this.numericUpDownRow_ValueChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(39, 103);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 49;
+            this.dataGridView2.Size = new System.Drawing.Size(465, 321);
+            this.dataGridView2.TabIndex = 36;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // listBoxSteps
+            // 
+            this.listBoxSteps.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBoxSteps.FormattingEnabled = true;
+            this.listBoxSteps.ItemHeight = 16;
+            this.listBoxSteps.Location = new System.Drawing.Point(522, 103);
+            this.listBoxSteps.Name = "listBoxSteps";
+            this.listBoxSteps.Size = new System.Drawing.Size(320, 324);
+            this.listBoxSteps.TabIndex = 37;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(397, 43);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(107, 50);
+            this.buttonCalculate.TabIndex = 38;
+            this.buttonCalculate.Text = "Рассчитать";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonCreateMas
+            // 
+            this.buttonCreateMas.Location = new System.Drawing.Point(263, 43);
+            this.buttonCreateMas.Name = "buttonCreateMas";
+            this.buttonCreateMas.Size = new System.Drawing.Size(107, 50);
+            this.buttonCreateMas.TabIndex = 39;
+            this.buttonCreateMas.Text = "Сформировать массив";
+            this.buttonCreateMas.UseVisualStyleBackColor = true;
+            this.buttonCreateMas.Click += new System.EventHandler(this.buttonCreateMas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 450);
+            this.Controls.Add(this.buttonCreateMas);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.listBoxSteps);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDownCol);
+            this.Controls.Add(this.numericUpDownRow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDownCol;
+        private System.Windows.Forms.NumericUpDown numericUpDownRow;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ListBox listBoxSteps;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Button buttonCreateMas;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MethodPotencialov
+{
+    public partial class Form1 : Form
+    {
+        struct Element
+        {
+            public int valueCost;
+            public bool check;
+        }
+
+        struct U
+        {
+            public int valueU;
+            public bool checkU;
+            public bool visible;
+        }
+
+        struct V
+        {
+            public int valueV;
+            public bool checkV;
+            public bool visible;
+        }
+
+        private Element[,] coefDelivery;
+        private int[,] IndirectTariffs;
+        private int[,] Estimate;
+
+        private U[] StocksU;
+        private V[] NeedsV;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView2.RowCount = 1;
+            dataGridView2.Columns[0].Width = 60;
+            buttonCalculate.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDownRow_ValueChanged(object sender, EventArgs e)
+        {
+            AddGridRowCol();
+        }
+
+        private void numericUpDownCol_ValueChanged(object sender, EventArgs e)
+        {
+            AddGridRowCol();
+        }
+
+        private void AddGridRowCol()
+        {
+
+            dataGridView2.RowCount = (int)numericUpDownRow.Value + 1;
+            dataGridView2.ColumnCount = (int)numericUpDownCol.Value + 1;
+
+            dataGridView2.Rows[0].DefaultCellStyle.BackColor = Color.Gray;
+            dataGridView2.Columns[0].DefaultCellStyle.BackColor = Color.Gray;
+
+            for (int j = 1; j < (dataGridView2.RowCount); j++)
+            {
+                for (int i = 1; i < dataGridView2.ColumnCount; i++)
+                {
+                    dataGridView2.Columns[i].HeaderText = string.Format("b" + i.ToString());
+                    dataGridView2.Rows[j].HeaderCell.Value = string.Format("a" + (j).ToString());
+                }
+            }
+
+            foreach (DataGridViewColumn column in dataGridView2.Columns)
+                column.Width = 60;
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var colorVal = dataGridView2.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor;
+            if (colorVal != Color.GreenYellow)
+            {
+                dataGridView2.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.GreenYellow;
+                coefDelivery[e.RowIndex, e.ColumnIndex].check = true;
+            }
+            else 
+            {
+                dataGridView2.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.White;
+                coefDelivery[e.RowIndex, e.ColumnIndex].check = false;
+            }
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            StocksU = new U[dataGridView2.RowCount];
+            NeedsV = new V[dataGridView2.ColumnCount];
+
+            listBoxSteps.Items.Add("Потенциалы");
+            while (AllFalse())
+            {
+                FindUV(StocksU, NeedsV);
+            }
+            listBoxSteps.Items.Add(" ");
+            listBoxSteps.Items.Add("Косвенные тарифы");
+            FindIndirectTariffs();
+            listBoxSteps.Items.Add(" ");
+            listBoxSteps.Items.Add("Оценки");
+            FindEstimate();
+        }
+
+        private bool AllFalse() 
+        {
+            bool checkAll = false;
+
+            for (int a = 1; a < (StocksU.Length); a++) 
+            {
+                if (!StocksU[a].checkU) 
+                {
+                    checkAll = true;
+                }
+            }
+
+            for (int b = 1; b < (NeedsV.Length); b++)
+            {
+                if (!NeedsV[b].checkV)
+                {
+                    checkAll = true;
+                }
+            }
+
+            return checkAll;
+        }
+
+        private void FindEstimate() 
+        {
+            int count = 0;
+
+            Estimate = new int[dataGridView2.RowCount, dataGridView2.ColumnCount];
+
+            for (int a = 1; a < (dataGridView2.RowCount); a++)
+            {
+                for (int b = 1; b < dataGridView2.ColumnCount; b++)
+                {
+                    if (!coefDelivery[a, b].check)
+                    {
+                        Estimate[a, b] = coefDelivery[a, b].valueCost - IndirectTariffs[a, b];
+                        listBoxSteps.Items.Add($"Оценка ^'[{a}][{b}] = {coefDelivery[a, b].valueCost} - {IndirectTariffs[a, b]} = " + Estimate[a, b]);
+                    }
+                }
+            }
+
+            for (int i = 1; i < Estimate.GetLength(0); i++) 
+            {
+                for (int j = 1; j < Estimate.GetLength(1); j++) 
+                {
+                    if (Estimate[i,j] < 0) 
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            if (count > 0)
+            {
+                listBoxSteps.Items.Add("НЕОБХОДИМО ПРОДОЛЖИТЬ ПОИСК");
+                MessageBox.Show("НЕОБХОДИМО ПРОДОЛЖИТЬ ПОИСК");
+            }
+            else 
+            {
+                listBoxSteps.Items.Add("ПОЛУЧЕН ОПТИМАЛЬНЫЙ ПЛАН");
+                MessageBox.Show("ПОЛУЧЕН ОПТИМАЛЬНЫЙ ПЛАН");
+            }
+        }
+
+        private void FindIndirectTariffs()
+        {
+            IndirectTariffs = new int[dataGridView2.RowCount, dataGridView2.ColumnCount];
+
+            for (int a = 1; a < (dataGridView2.RowCount); a++)
+            {
+                for (int b = 1; b < dataGridView2.ColumnCount; b++)
+                {
+                    if (!coefDelivery[a, b].check)
+                    {
+                        IndirectTariffs[a, b] = StocksU[a].valueU + NeedsV[b].valueV;
+                        listBoxSteps.Items.Add($"Косвенный тариф C'[{a}][{b}] = {StocksU[a].valueU} + {NeedsV[b].valueV} = {IndirectTariffs[a, b]}");
+                    }
+                }
+            }
+        }
+
+        private void FindUV(U[] StocksU, V[] NeedsV)
+        {
+            for (int a = 1; a < (dataGridView2.RowCount); a++)
+            {
+                for (int b = 1; b < dataGridView2.ColumnCount; b++)
+                {
+                    if (coefDelivery[a, b].check && !NeedsV[b].checkV && !StocksU[a].checkU) 
+                    {
+                        if (!CheckcoefDeliveryNext())
+                        {
+                            StocksU[a].valueU = 0;
+                            StocksU[a].checkU = true;
+                            if (!StocksU[a].visible)
+                            {
+                                listBoxSteps.Items.Add($"Потенциал U{a} = {StocksU[a].valueU}");
+                                StocksU[a].visible = true;
+                            }
+                        }
+                    }
+                    
+                    if (coefDelivery[a, b].check && !NeedsV[b].checkV) 
+                    {
+                        if (StocksU[a].checkU)
+                        {
+                            NeedsV[b].valueV = coefDelivery[a, b].valueCost - StocksU[a].valueU;
+                            NeedsV[b].checkV = true;
+                            if (!NeedsV[b].visible)
+                            {
+                                listBoxSteps.Items.Add($"Потенциал V{b} = {NeedsV[b].valueV}");
+                                NeedsV[b].visible = true;
+                            }
+                            continue;
+                        }
+                    }
+
+                    if (coefDelivery[a, b].check && NeedsV[b].checkV) 
+                    {
+                        StocksU[a].valueU = coefDelivery[a, b].valueCost - NeedsV[b].valueV;
+                        StocksU[a].checkU = true;
+                        if (!StocksU[a].visible)
+                        {
+                            listBoxSteps.Items.Add($"Потенциал U{a} = {StocksU[a].valueU}");
+                            StocksU[a].visible = true;
+                        }
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private bool CheckcoefDeliveryNext() 
+        {
+            int count = 0;
+
+            for (int a = 1; a < (dataGridView2.RowCount); a++)
+            {
+                for (int b = 1; b < dataGridView2.ColumnCount; b++)
+                {
+                    if (coefDelivery[a, b].check && (StocksU[a].checkU || NeedsV[b].checkV))
+                    {
+                        if (StocksU[a].checkU) 
+                        {
+                            NeedsV[b].valueV = coefDelivery[a, b].valueCost - StocksU[a].valueU;
+                            NeedsV[b].checkV = true;
+                            if (!NeedsV[b].visible) 
+                            {
+                                listBoxSteps.Items.Add($"Потенциал V{b} = {NeedsV[b].valueV}");
+                                NeedsV[b].visible = true;
+                                count++;
+                            }
+                        }
+
+                        if (NeedsV[b].checkV)
+                        {
+                            StocksU[a].valueU = coefDelivery[a, b].valueCost - NeedsV[b].valueV;
+                            StocksU[a].checkU = true;
+                            if (!StocksU[a].visible) 
+                            {
+                                listBoxSteps.Items.Add($"Потенциал U{a} = {StocksU[a].valueU}");
+                                StocksU[a].visible = true;
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (count >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void createMatrix()
+        {
+            coefDelivery = null;
+
+            coefDelivery = new Element[dataGridView2.RowCount, dataGridView2.ColumnCount];
+
+            for (int i = 1; i < dataGridView2.RowCount; i++)
+            {
+                for (int j = 1; j < dataGridView2.ColumnCount; j++)
+                {
+                    coefDelivery[i, j].valueCost = Convert.ToInt32(dataGridView2.Rows[i].Cells[j].Value);
+                    var colorVal = dataGridView2.Rows[i].Cells[j].Style.BackColor;
+                    if (colorVal == Color.GreenYellow)
+                    {
+                        coefDelivery[i, j].check = true;
+                    }
+                }
+            }
+        }
+
+        private void buttonCreateMas_Click(object sender, EventArgs e)
+        {
+            createMatrix();
+            buttonCalculate.Enabled = true;
+        }
+    }
+}
